292673a88cecc0b9ba5ef8fdc12c9c1da9d6e23a
Takandrew <andreikaril@mail.ru>
2024-05-30 20:56:17 +0300

    fix: changed getScoreSet function name

--- src/api/getScoreSet.jsx
+++ src/api/getScoreSet.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import config from "/config.json";
 
-export default async function getGPTRecipeQuestions(recipeScoreModel) {
+export default async function getScoreSet(recipeScoreModel) {
   try {
     const response = await axios.post(
       `${config.server.address}api/recipes/scores/set/`,
